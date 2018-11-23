119e0181a3dc6550fb1624e98cad547396e2b8b1
smallzz <836227922@qq.com>
2018-11-23 16:07:50 +0800

    a

--- config/database.php
+++ config/database.php
@@ -12,7 +12,7 @@
         // 数据库类型
         'type'            => 'mysql',
         // 服务器地址
-        'hostname'        => 'locahost',
+        'hostname'        => '119.23.72.99',
         // 数据库名
         'database'        => 'staff',
         // 用户名
