f418d530b1c87523346b8ec26f3495b950128a68
smallzz <836227922@qq.com>
2018-12-17 13:36:44 +0800

    as

---
+++ application/index/controller/Puser.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smallzz
+ * Date: 2018/12/17
+ * Time: 下午1:29
+ */
+
+namespace app\index\controller;
+
+
+use app\index\model\CoordinatesModel;
+use think\Controller;
+
+class Puser extends Controller
+{
+    public function pu(){
+        $param = $this->request->param();
+        $puser = new CoordinatesModel();
+        $data = [
+            'phone'=>'13260324342',
+            'lon'=>'78.123213',
+            'lat'=>'132.603243',
+        ];
+        $puser->save($data);
+    }
+}
---
+++ application/index/model/CoordinatesModel.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smallzz
+ * Date: 2018/12/17
+ * Time: 下午1:33
+ */
+
+namespace app\index\model;
+
+
+use think\Model;
+
+class CoordinatesModel extends Model
+{
+    protected $table = 'pp_coordinates';
+
+
+}
